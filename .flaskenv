821a58944911fc959b2a2801041b71c325b2af2f
frems24 <wg@pwr.edu.pl>
2018-10-25 17:18:38 +0200

    v 0.0.1 First and minimal version Added:     - models: User, Post     - views: index, login, logout, register, user, edit_profile     - forms: login, registration, edit_profile     - corresponding html templates     - db migrations     - basic config.py and .flaskenv

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=ang4us.py
+FLASK_ENV=development
+FLASK_DEBUG=1
---
+++ app/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from app import db
+
+
+class User(UserMixin, db.Model):
+    """ 'user' table in database
+        class UserMixin adds: is_authenticated, is_active, is_anonymous, get_id()
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    """ 'post' table in database """
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
