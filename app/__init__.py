f12a7a3ee9702e90c23ff2f5727da10a3b6103a0
frems24 <wg@pwr.edu.pl>
2018-10-30 19:40:16 +0100

    v 0.0.2_1 Added API:     - get_user(id)     - get_users

--- app/__init__.py
+++ app/__init__.py
@@ -17,5 +17,7 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
 
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
 
 from app import models
--- app/models.py
+++ app/models.py
@@ -1,11 +1,39 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import url_for
 from flask_login import UserMixin
 from app import login
 from app import db
 
 
-class User(UserMixin, db.Model):
+class PaginatedApiMixin:
+    """
+    Klasa, umożliwiająca podzielenie wyników zapytania na strony.
+    Wzięta z rozdziału ostatniego o API
+    Na podstawie podobnego rozwiązania z rozdziału 16
+    Na razie wpisałem część funkcjonalności...
+    """
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, error_out=False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page, **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+
+class User(PaginatedApiMixin, UserMixin, db.Model):
     """ 'user' table in database
         class UserMixin adds: is_authenticated, is_active, is_anonymous, get_id()
     """
@@ -25,6 +53,29 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'about_me': self.about_me,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            '_links': {
+                'self': url_for('api.get_user', id=self.id)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
 
 
 class Post(db.Model):
