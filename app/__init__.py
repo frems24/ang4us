be352e4a86d3613728b1d51637c31fb791c44194
frems24 <wg@pwr.edu.pl>
2018-11-05 19:19:17 +0100

    v 0.0.5 Added:     - Logging to a file when debug is off Fixed:     - Unique username duplication error in User model (web form)

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -24,3 +27,14 @@ from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
 
 from app import models
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/ang4us.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('ang4us startup')
